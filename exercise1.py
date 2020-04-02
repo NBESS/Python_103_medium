a8fc5c64f1b4fb8de29958d38a82faff305dbc69
NBESS <nicholasdbess@gmail.com>
2020-04-02 19:33:32 -0400

    adds comments

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+# Calculate amount of a tip to leave at a restaurant.
+# Prompt the user for two things, total bill amount and level of service
+
+# Setup
+
+
+# Calculate the tip and determine the level of service
+# levels of service: good -> 20% , fair -> 15% , bad -> 10%
+
+
+
+# Output the tip amount based on level of service
