9a19bc132582ab67419a026692b9083ae27badf5
NBESS <nicholasdbess@gmail.com>
2020-04-02 19:54:09 -0400

    adds variables

--- exercise1.py
+++ exercise1.py
@@ -2,11 +2,14 @@
 # Prompt the user for two things, total bill amount and level of service
 
 # Setup
-
+bill = float(input('How much is your bill? '))
+service_message = 'Good, Fair, or Bad'
+level_of_service = input(f'How was your service?\n{service_message}').lower()
 
 # Calculate the tip and determine the level of service
 # levels of service: good -> 20% , fair -> 15% , bad -> 10%
-
+total_to_pay =
 
 
 # Output the tip amount based on level of service
+print("%.2f" % total_to_pay)
