5dfad27e195d2f4f29a15dbe8e2c2ff7631495e6
NBESS <nicholasdbess@gmail.com>
2020-04-05 16:19:51 -0400

    adds completed code

--- exercise5.py
+++ exercise5.py
@@ -7,15 +7,22 @@
 char_on_each_side = int(input('How many characters on each side of center_character? '))
 
 # Do the work
-center_char = '*'
-left_end_char = char_on_each_side * center_char
-right_end_char = char_on_each_side * center_char
 
 count = 0
 while count < 1:
+    center_char = '*'
     end_char = " "
+    left_end_char = char_on_each_side * end_char
+    right_end_char = char_on_each_side * end_char    
     print(f'{left_end_char}{center_char}{right_end_char}')
-
-
-
-# Print the output
+    levels = 0
+    space_on_side = char_on_each_side
+    new_center_char = '*'
+    while levels < char_on_each_side:
+        space_on_side -= 1
+        left_end_char = space_on_side * end_char
+        right_end_char = space_on_side * end_char         
+        print(f'{left_end_char}{new_center_char}{center_char}{new_center_char}{right_end_char}')
+        new_center_char += '*'
+        levels += 1
+    count += 1
